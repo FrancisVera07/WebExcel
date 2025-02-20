5837e29ef2a389829b4da30216656cf505e2d1a5
FrancisVera07 <francisco.vera.1937@gmail.com>
2025-02-20 15:20:54 -0600

    feat(development): Creación de la vista para subir el archivo Excel

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,58 @@
-function App() {
+import { useDropzone } from "react-dropzone"
+import {FileDropzone} from "./components/FileDropzone.jsx"
+import {FileInfo} from "./components/FileInfo.jsx"
+import {FileActions} from "./components/FileActions.jsx"
+import {FileTable} from "./components/FileTable.jsx"
+import {userFileUpload} from "./hooks/userFileUpload.js"
+
+const App = () => {
+
+    // Variables de la función userFileUpload()
+    const {
+        fileData,
+        fileName,
+        error,
+        onDrop,
+        setFileData,
+        setFileName,
+        setError
+    } = userFileUpload()
+
+    // Extenciones permitidas
+    const { getRootProps, getInputProps } = useDropzone({
+        onDrop,
+        accept: ".xlsx,.xls",
+    })
+
+    /**
+     * Función para guardar el archivo
+     */
+    const handleUpload = () => {
+        if (fileData) {
+            alert("Archivo guardado como JSON con éxito")
+        }
+    }
+
+    /**
+     * Función para eliminar el archivo
+     */
+    const handleDelete = () => {
+        setFileData(null)
+        setFileName("")
+        setError("")
+    }
+
     return (
-        <div className={"flex items-center justify-center h-screen"}>
-            <h1 className={"text-3xl font-bold underline"}>
-                Upload your Excel file
-            </h1>
+        <div className="p-4 space-y-4">
+            <FileDropzone getInputProps={getInputProps} getRootProps={getRootProps} />
+            <FileInfo fileName={fileName} error={error} />
+
+            {fileData && (
+                <>
+                    <FileActions handleUpload={handleUpload} handleDelete={handleDelete} />
+                    <FileTable fileData={fileData} />
+                </>
+            )}
         </div>
     )
 }
